69f0d806dc5fae0118fc44b09af5bc7dce41b77b
Venci <venci34@gmail.com>
2021-12-22 13:28:58 +0200

    code added to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,8 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
+
+const img = document.createElement('IMG');
+img.setAttribute('src', 'images/flame.png');
+document.querySelectorAll('.hot').appendChild(img);
+console.log(img);
