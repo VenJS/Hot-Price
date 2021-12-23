e9cae57d9926a1422d5e42268ad5ae1db1c9fbf9
Venci <venci34@gmail.com>
2021-12-23 10:31:46 +0200

    asdasdsa

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
-import "../scss/app.scss";
+
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
 
-const img = document.createElement('IMG');
-img.setAttribute('src', 'images/flame.png');
-document.querySelectorAll('.price hot').appendChild(img);
-console.log(img);
+const hotPriceDiv = document.querySelectorAll('.hot');
+const image = document.createElement('img');
+image.src = 'images/flame.png';
+hotPriceDiv.appendChild(image);
+
